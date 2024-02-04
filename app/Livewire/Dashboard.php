c5de082f2f64036131c1499a9b7c51802af1bd51
Danis Yogaswara <danistutorial@gmail.com>
2024-02-05 00:56:07 +0700

    FEATURE: - Count In Dashboard

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,12 +2,42 @@
 
 namespace App\Livewire;
 
+use App\Models\Flag;
+use App\Models\FlagCount;
+use App\Models\Legislative;
+use App\Models\LegislativeCount;
+use App\Models\Village;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard');
+        return view('livewire.dashboard', [
+            'jumlah_suara_masuk' => Village::sum('jumlah_suara_masuk'),
+            'jumlah_suara_sah' => Village::sum('jumlah_suara_sah'),
+            'jumlah_suara_tidak_sah' => Village::sum('jumlah_suara_tidak_sah'),
+            'flags' => $this->sortFlag(),
+            'legislatives' => $this->sortLegislative(),
+            'sum_all_count_flag' => FlagCount::sum('count'),
+            'sum_all_count_legislative' => LegislativeCount::sum('count')
+        ]);
     }
+
+    public function sortFlag()
+    {
+        $flag = Flag::get();
+        return $flag->sortByDesc(function ($flag) {
+            return $flag->flagCount->sum('count');
+        })->take(5);
+    }
+
+    public function sortLegislative()
+    {
+        $legislative = Legislative::get();
+        return $legislative->sortByDesc(function ($legislative) {
+            return $legislative->legislativeCount->sum('count');
+        });
+    }
+    
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,7 +1,7 @@
 <div>
     <livewire:navbar>
         <div class="container mx-auto mt-10 px-10">
-            @role('user')
+            @if(auth()->user()->roles[0]->name == 'user')
             <nav class="flex" aria-label="Breadcrumb">
                 <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
                     <li class="inline-flex items-center">
@@ -19,6 +19,126 @@
 
             </div>
             <livewire:input-form>
-            @endrole
+                @else
+                <div class="grid w-full grid-cols-1 gap-4 mt-4 xl:grid-cols-3 2xl:grid-cols-3">
+                    <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                        <div class="w-full">
+                            <h3 class="text-base font-normal text-gray-500 dark:text-gray-400">
+                                Jumlah Suara Masuk
+                            </h3>
+                            <span class="text-2xl font-bold leading-none text-gray-900 sm:text-3xl dark:text-white" wire:poll.5s>{{$jumlah_suara_masuk}}</span>
+                        </div>
+                    </div>
+                    <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                        <div class="w-full">
+                            <h3 class="text-base font-normal text-gray-500 dark:text-gray-400">
+                                Jumlah Suara Sah
+                            </h3>
+                            <span class="text-2xl font-bold leading-none text-gray-900 sm:text-3xl dark:text-white" wire:poll.5s>{{$jumlah_suara_sah}}</span>
+                        </div>
+                    </div>
+                    <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                        <div class="w-full">
+                            <h3 class="text-base font-normal text-gray-500 dark:text-gray-400">
+                                Jumlah Suara Tidak Sah
+                            </h3>
+                            <span class="text-2xl font-bold leading-none text-gray-900 sm:text-3xl dark:text-white" wire:poll.5s>{{$jumlah_suara_tidak_sah}}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="py-10 grid w-full grid-cols-1 gap-4 mt-4 xl:grid-cols-2 2xl:grid-cols-3">
+                    <!-- Partai -->
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <h4 class="py-4 px-4 text-2md font-bold dark:text-white">5 Partai Jumlah Suara Terbanyak</h4>
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        NO URUT
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Logo
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Nama Partai
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Jumlah Suara
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Persentase
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody wire:pool.5s>
+                            @if($flags->count() > 0)
+                            @foreach($flags as $key => $value)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{$value->no_urut}}
+                                </th>
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <img src="{{url('/images')}}/{{$value->logo}}" alt="" width="42px">
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{$value->nama_partai}}
+                                </td>
+                                <td class="px-6 py-4" wire:poll.5s>
+                                    {{$value->flagCount->sum('count')}}
+                                </td>
+                                <td class="px-6 py-4" wire:poll.5s>
+                                {{ number_format(($value->flagCount->sum('count') / $sum_all_count_flag) * 100,2,",",".") }} %
+                                </td>
+                            </tr>
+                            @endforeach
+                            @endif
+                        </tbody>
+                        </table>
+                    </div>
+
+                    <!-- Calon -->
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <h4 class="py-4 px-4 text-2md font-bold dark:text-white">Nominasi Suara Terbanyak</h4>
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    No Urut
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Nama Calon DPR
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Jumlah Suara
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Persentase
+                                </th>
+                                </tr>
+                            </thead>
+                            <tbody wire:pool.5s>
+                            @if($legislatives->count() > 0)
+                            @foreach($legislatives as $key => $value)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{$value->no_urut}}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{$value->nama_calon}}
+                                </td>
+                                <td class="px-6 py-4" wire:poll.5s>
+                                    {{ $value->legislativeCount->sum('count') }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ number_format(($value->legislativeCount->sum('count') / $sum_all_count_legislative) * 100, 2,",",".")}} %
+                                </td>
+                            </tr>
+                            @endforeach
+                            @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                @endif
         </div>
 </div>
