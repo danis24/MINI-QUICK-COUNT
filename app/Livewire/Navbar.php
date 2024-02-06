b05d43f99eb04eac07b7f6d9e4b341699621859c
Danis Yogaswara <danistutorial@gmail.com>
2024-02-06 08:05:34 +0700

    FIX NAVBAR

--- app/Livewire/Navbar.php
+++ app/Livewire/Navbar.php
@@ -6,7 +6,7 @@ use Livewire\Component;
 
 class Navbar extends Component
 {
-    public $navActive = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
+    public $navActive = "text-white mx-3 my-10 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700";
     public $navLink = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
     public function render()
     {
